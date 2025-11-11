bbdeb0580386c052fa0eb10a85c8e43c6097d1da
Danila <75013666+YakushevDanila@users.noreply.github.com>
2025-11-11 18:04:22 +0300

    Delete SheBot/bot directory

--- SheBot/bot/config.py
+++
@@ -1,9 +0,0 @@
-BOT_TOKEN = "7906790313:AAEcwGsxLotreBkOTcT9SuVj0ij8DqIAuig"
-ANNA_ID = 462439834  # Telegram ID Àííû
-
-# ID Google òàáëèöû (èç URL)
-# Ïðèìåð: https://docs.google.com/spreadsheets/d/1AbCdEfGhiJKlMnOpQrStuVWxyz123/edit
-SHEET_ID = "1mHC7Wdp8Xwpq4taZ8RE6PXiePUISW7eUDworN_Daf3g"
-
-TIMEZONE = "Europe/Moscow"
-DB_PATH = "data.db"
