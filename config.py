1ce98c980badf53b2042904214ee87de03b5542d
Danila <75013666+YakushevDanila@users.noreply.github.com>
2025-11-11 18:41:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-BOT_TOKEN = "7906790313:AAEcwGsxLotreBkOTcT9SuVj0ij8DqIAuig"
-ANNA_ID = 960011769  # Telegram ID Анны
+BOT_TOKEN = "8557065807:AAF2VyjGDpEiOVua5N0YvOlBgvxzUWQ45zo"
+ANNA_ID = 960011769  # Telegram ID ГЂГ­Г­Г»
 
-# ID Google таблицы (из URL)
-# Пример: https://docs.google.com/spreadsheets/d/1AbCdEfGhiJKlMnOpQrStuVWxyz123/edit
+# ID Google ГІГ ГЎГ«ГЁГ¶Г» (ГЁГ§ URL)
+# ГЏГ°ГЁГ¬ГҐГ°: https://docs.google.com/spreadsheets/d/1AbCdEfGhiJKlMnOpQrStuVWxyz123/edit
 SHEET_ID = "1mHC7Wdp8Xwpq4taZ8RE6PXiePUISW7eUDworN_Daf3g"
 
 TIMEZONE = "Europe/Moscow"
+
 DB_PATH = "data.db"
